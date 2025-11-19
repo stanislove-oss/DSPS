dbd8597e38f5351d9ff694cccf351beed4d069c6
stanislav <stanislav_vatavu@mail.ru>
2025-11-19 18:12:31 +0300

    Add files to repo

---
+++ some_file.py
@@ -0,0 +1,16 @@
+'''This is a test docstring'''
+import random
+
+def calculate(x, kind):
+    '''This is a function docstring'''
+    if kind == 'square':
+        return x**2
+    if kind == 'double':
+        return x * 2
+    if kind == 'hello':
+        return 'Hello World'
+    return 1
+
+X=0
+Y=10
+print(random.randint(X, Y))
